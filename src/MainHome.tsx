334dcf98bb0e6e8df4e9309fb39a4d951f63ddfe
kkevi <borakim@simbaat.com>
2022-12-23 16:29:11 +0900

    animation2

---
+++ src/MainHome.tsx
@@ -0,0 +1,7 @@
+// import Transform from "./transform";
+import Animation from "./animation";
+import React from "react";
+
+export default function MainHome() {
+  return <Animation />;
+}
---
+++ src/animation/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../../styles/Animation.module.css";
+
+export default function Animation() {
+  return (
+    <div>
+      <h1>Animation</h1>
+      <div className={styles.box}>box</div>
+    </div>
+  );
+}
